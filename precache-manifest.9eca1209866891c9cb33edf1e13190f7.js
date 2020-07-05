c1ae3bdb1df43ea4f70667a08b28f85cce6c77d4
shivang02 <shivang.dubey1@student.amity.edu>
2020-07-05 19:05:56 +0530

    Updates

---
+++ precache-manifest.9eca1209866891c9cb33edf1e13190f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8529f565e35e829582f60dc784ddc77d",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "6b0551640e8b98c0d968",
+    "url": "/random-quote-generator/static/css/main.870b4c97.chunk.css"
+  },
+  {
+    "revision": "1a491193b40d0ece7700",
+    "url": "/random-quote-generator/static/js/2.1f818972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.1f818972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0551640e8b98c0d968",
+    "url": "/random-quote-generator/static/js/main.f794489d.chunk.js"
+  },
+  {
+    "revision": "efbd8013faa4b47d47b4",
+    "url": "/random-quote-generator/static/js/runtime-main.04b377e6.js"
+  }
+]);
